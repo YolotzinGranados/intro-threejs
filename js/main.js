3431bdcc7f517118ab0ea60601330028cae3df7d
YolotzinGranados <yolotzin.gm15@gmail.com>
2025-03-30 23:30:11 -0600

    Segunda  escena 3D

--- js/main.js
+++ js/main.js
@@ -9,12 +9,12 @@ document.body.appendChild(renderer.domElement);
 
 // Crear cubo sólido
 const geometry = new THREE.BoxGeometry(1, 1, 1, 9, 9, 9);
-const material = new THREE.MeshBasicMaterial({ color:"rgb(0,0,0)" });
+const material = new THREE.MeshBasicMaterial({ color: "rgb(0,0,0)" });
 const cube = new THREE.Mesh(geometry, material);
 
 // Crear líneas de los triángulos individuales
 const wireframeGeometry = new THREE.WireframeGeometry(geometry);
-const wireframeMaterial = new THREE.LineBasicMaterial({ color:"rgb(255,255,255)" }); 
+const wireframeMaterial = new THREE.LineBasicMaterial({ color: "rgb(255,255,255)" });
 const wireframe = new THREE.LineSegments(wireframeGeometry, wireframeMaterial);
 
 // Agrupar cubo y su wireframe
@@ -23,7 +23,10 @@ group.add(cube);
 group.add(wireframe);
 scene.add(group);
 
-camera.position.z = 5;
+camera.position.x = 5;  // Posiciona la cámara en el eje X
+camera.position.y = 0;  // Mantiene la cámara a nivel del eje Y
+camera.position.z = 0;  // Deja la cámara sobre el eje X
+camera.lookAt(0, 0, 0); // Hace que la cámara mire hacia el origen (donde está el cubo)
 
 function animate() {
   group.rotation.x += 0.01;
